43f9cd668e807db8d8c71ba5df576b615041ac1c
xifeng-jin <52062228+xifeng-jin@users.noreply.github.com>
2026-05-09 08:12:50 +0000

    feat: add husky for outer

--- my-app/src/component/button/index.tsx
+++ my-app/src/component/button/index.tsx
@@ -1,13 +1,9 @@
-import BasicImg from "../imgs/BasicImg";
+
 const MyButton = () => {
     return (
         <div>button</div>
     )
 }
-
-export const a = 'jjj';
-
-export const b = 'ddd';
 
 const a2 = 'jjj';
 
